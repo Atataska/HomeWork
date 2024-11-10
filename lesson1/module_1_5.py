ffe4c419ef42ec3fbb63601efddfcc1a62405689
Atataska <valen46vla@gmail.com>
2024-11-10 14:50:53 +0300

    Неизменяемые и изменяемые объекты. Кортежи
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"
    
    Цель:
    Написать программу на языке Python, используя Pycharm, для работы с неизменяемыми и изменяемыми объектами.
    
    1. В проекте, где вы решаете домашние задания, создайте модуль 'module_1_5.py' и напишите весь код в нём.
    
    2. Задайте переменные разных типов данных:
      - Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
      - Выполните операции вывода кортежа immutable_var на экран.
    
    3. Изменение значений переменных:
       - Попытайтесь изменить элементы кортежа immutable_var. Объясните, почему нельзя изменить значения элементов кортежа.
    
    4. Создание изменяемых структур данных:
        - Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
        - Измените элементы списка mutable_list.
        - Выведите на экран измененный список mutable_list.
    
    Примечания:
    - Для вывода значений на экран используйте функцию print().
    - Обратите внимание на особенности изменяемых и неизменяемых объектов в Python.
    
    Пример результата выполнения программы:
    Immutable tuple: (1, 2, 'a', 'b')
    Mutable list: [1, 2, 'a', 'b', 'Modified']

---
+++ lesson1/module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = tuple ([1, 2, 3, 5, "Привет"])
+print (immutable_var)
+#immutable_var[2] = 20
+# Кортеж (tuple) в Python — неизменяемый тип, поэтому после создания его элементы нельзя удалять или менять.
+mutable_list = [1, 2,3,4,5]
+mutable_list [0]=5
+print (mutable_list)
