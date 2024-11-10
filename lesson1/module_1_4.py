ed2a88ff249ef59a2f8b0cd2511da8353586882f
Atataska <valen46vla@gmail.com>
2024-11-10 14:22:42 +0300

    Организация программ и методы строк
    Практическая работа по уроку "Организация программ и методы строк"
    
    Цель: Написать программу на языке Python с использованием Pycharm для работы с методами строк и организации программ.
    
    1. В проекте, где вы решаете домашние задания, создайте модуль 'module_1_4.py' и напишите весь код в нём.
    
    2. Организуйте программу:
    
    Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
    Вывести количество символов введённого текста
    3. Работа с методами строк:
    
    Используя методы строк, выполните следующие действия:
    
    Выведите строку my_string в верхнем регистре.
    Выведите строку my_string в нижнем регистре.
    Измените строку my_string, удалив все пробелы.
    Выведите первый символ строки my_string.
    Выведите последний символ строки my_string.
    Примечания:
    
    Для вывода значений на экран используйте функцию print().
    Для вызова методов строк используйте операцию точки '.'.
    Дополнительно о всех методах строк можно узнать здесь.
    Пример результата выполнения программы:
    
    Код:
    name = input()
    print(name.upper())
    
    Ввод в консоль:
    Urban
    
    Вывод на консоль:
    URBAN

---
+++ lesson1/module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('Добрый день, ввидите свое имя и отчество:' )
+
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print (my_string.lower())
+
+my_string = my_string.replace(" ", "")
+print(my_string)
+
+print (my_string[0])
+print (my_string[-1])
