f4cc987957dd2d94727589a359b7570966b6b3d1
Atataska <valen46vla@gmail.com>
2024-11-10 12:59:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello world')
+
+# - "1st program".
+print(9**0.5*5)
+
+# - "2nd program".
+print( 9.99 > 9.98 and 1000 != 1000.1)
+
+# - "3rd program".
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+# - "4th program".
+a = '123.456'
+a = float(a)
+y = a * 10
+print (int(y)%10)
